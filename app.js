bd2c7c8b6c5dda6a5d68be225747d1e2226e1bde
SudeepMi <sudeepm0599@gmail.com>
2021-02-09 19:21:34 +0545

    node setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8080;
+// const host = process.env.HOSTNAME || "codewithsudeep";
+const host = process.env.HOSTNAME || "0.0.0.0";
+
+
+const server = app.listen(port, host, () => {
+  console.log('Port %d',port);
+  console.log(`Node.js API server is listening on http://${host}`);
+});
+
+
+
+
+
+
+//serve static asset 
+
+
+
+
+
+
+
